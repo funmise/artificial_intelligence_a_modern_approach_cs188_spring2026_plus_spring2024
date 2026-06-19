b97acaf03ba05b9dca2b1836312f8fc3345ccb1e
funmise <42499166+funmise@users.noreply.github.com>
2026-06-19 04:08:27 -0600

    Add solutions for project0

--- projects/project0-tutorial/shopSmart.py
+++ projects/project0-tutorial/shopSmart.py
@@ -4,7 +4,7 @@
 # educational purposes provided that (1) you do not distribute or publish
 # solutions, (2) you retain this notice, and (3) you provide clear
 # attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
-# 
+#
 # Attribution Information: The Pacman AI projects were developed at UC Berkeley.
 # The core projects and autograders were primarily created by John DeNero
 # (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
@@ -20,27 +20,41 @@ Welcome to shop2 fruit shop
 For orders:  [('apples', 1.0), ('oranges', 3.0)] best shop is shop1
 For orders:  [('apples', 3.0)] best shop is shop2
 """
+
 from __future__ import print_function
+
 import shop
 
 
 def shopSmart(orderList, fruitShops):
     """
-        orderList: List of (fruit, numPound) tuples
-        fruitShops: List of FruitShops
+    orderList: List of (fruit, numPound) tuples
+    fruitShops: List of FruitShops
     """
-    "*** YOUR CODE HERE ***"
-    return None
+    best_shop = None
+    cheapest_total = None
+
+    for fruit_shop in fruitShops:
+        curr_total = fruit_shop.getPriceOfOrder(orderList)
+        if cheapest_total is None or curr_total < cheapest_total:
+            best_shop = fruit_shop
+            cheapest_total = curr_total
+
+    return best_shop
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     "This code runs when you invoke the script from the command line"
-    orders = [('apples', 1.0), ('oranges', 3.0)]
-    dir1 = {'apples': 2.0, 'oranges': 1.0}
-    shop1 = shop.FruitShop('shop1', dir1)
-    dir2 = {'apples': 1.0, 'oranges': 5.0}
-    shop2 = shop.FruitShop('shop2', dir2)
+    orders = [("apples", 1.0), ("oranges", 3.0)]
+    dir1 = {"apples": 2.0, "oranges": 1.0}
+    shop1 = shop.FruitShop("shop1", dir1)
+    dir2 = {"apples": 1.0, "oranges": 5.0}
+    shop2 = shop.FruitShop("shop2", dir2)
     shops = [shop1, shop2]
-    print("For orders ", orders, ", the best shop is", shopSmart(orders, shops).getName())
-    orders = [('apples', 3.0)]
-    print("For orders: ", orders, ", the best shop is", shopSmart(orders, shops).getName())
+    print(
+        "For orders ", orders, ", the best shop is", shopSmart(orders, shops).getName()
+    )
+    orders = [("apples", 3.0)]
+    print(
+        "For orders: ", orders, ", the best shop is", shopSmart(orders, shops).getName()
+    )
